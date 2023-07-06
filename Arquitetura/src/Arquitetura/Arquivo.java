2f321163c6702b198bb97e0437c7e1e84c7be23e
isaac <isaac@10.2.171.89>
2023-07-06 18:20:33 -0300

    Classe Arquivo. Faz a leitura do arquivo "MemoriaPrincipal". Arquivo "MemoriaPrincipal.txt"

---
+++ Arquitetura/src/Arquitetura/Arquivo.java
@@ -0,0 +1,24 @@
+package Arquitetura;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Arquivo {
+    public static void main(String[] args) {
+    	
+    	String nomeArquivo = "MemoriaPrincipal.txt";
+
+    	        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+    	            String linha;
+
+    	            while ((linha = br.readLine()) != null) {
+    	                System.out.println(linha);
+    	            }
+    	        } catch (IOException e) {
+    	            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+    	        }
+    	    }
+    	}
+
+
